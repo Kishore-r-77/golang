b7a8a531ee3c92bd77f54c78781acf14b48ac5ba
Kishore-r-77 <kishorework77@gmail.com>
2023-12-13 12:35:58 +0530

    fetch client table and replaced Report with client

--- report-api/main.go
+++ report-api/main.go
@@ -14,52 +14,65 @@ import (
 var db *gorm.DB
 var err error
 
-type Report struct {
+type Client struct {
 	gorm.Model
-	Title   string
-	Content string
+	ClientShortName string `gorm:"type:varchar(50)"`
+	ClientLongName  string `gorm:"type:varchar(50)"`
+	ClientSurName   string `gorm:"type:varchar(50)"`
+	Gender          string `gorm:"type:varchar(05)"`
+	Salutation      string `gorm:"type:varchar(05)"`
+	Language        string `gorm:"type:varchar(05)"`
+	ClientDob       string `gorm:"type:varchar(8)"`
+	ClientDod       string `gorm:"type:varchar(8)"`
+	ClientEmail     string `gorm:"type:varchar(100)"`
+	ClientMobCode   string `gorm:"type:varchar(05)"`
+	ClientMobile    string `gorm:"type:varchar(20)"`
+	ClientStatus    string `gorm:"type:varchar(05)"`
+	ClientType      string `gorm:"type:varchar(01)"` // C CORPORATE I FOR INDIVIDUAL
+	NationalId      string `gorm:"type:varchar(50);unique"`
+	Nationality     string `gorm:"type:varchar(02)"`
 }
 
 func main() {
-	// Replace "user:password@tcp(host:port)/dbname?charset=utf8mb4&parseTime=True&loc=Local" with your MySQL connection string
+	
 	dsn := "root:root@tcp(127.0.0.1:3306)/policy?charset=utf8mb4&parseTime=True&loc=Local"
 
-	// Connect to the MySQL database
+	
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Migrate the schema
-	db.AutoMigrate(&Report{})
+	
+	// db.AutoMigrate(&Client{})
 
-	// Set up the Gin router
+	
 	router := gin.Default()
 
-	// Define API routes
-	router.GET("/reports", getReportsHandler)
-	router.POST("/reports", createReport)
-	router.GET("/generate-pdf-report", generatePDFReportHandler) // New endpoint for generating a PDF report
+	
+	router.GET("/clients", getClientsHandler)
+	router.POST("/clients", createClient)
+	router.GET("/generate-pdf-client", generatePDFClientHandler) // New endpoint for generating a PDF client
 
-	// Run the server
+	
 	port := 8080
 	router.Run(fmt.Sprintf(":%d", port))
 }
 
-// Handler to get all reports
-func getReportsHandler(c *gin.Context) {
-	reports, err := getReports(c)
+
+func getClientsHandler(c *gin.Context) {
+	clients, err := getClients(c)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, reports)
+	c.JSON(http.StatusOK, clients)
 }
 
-// Handler to create a new report
-func createReport(c *gin.Context) {
-	var input Report
+
+func createClient(c *gin.Context) {
+	var input Client
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -68,10 +81,9 @@ func createReport(c *gin.Context) {
 	c.JSON(http.StatusOK, input)
 }
 
-// Handler to generate a PDF report
-// Handler to generate a PDF report
-func generatePDFReportHandler(c *gin.Context) {
-	reports, err := getReports(c)
+
+func generatePDFClientHandler(c *gin.Context) {
+	clients, err := getClients(c)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
@@ -80,14 +92,18 @@ func generatePDFReportHandler(c *gin.Context) {
 	pdf := gofpdf.New("P", "mm", "A4", "")
 	pdf.AddPage()
 	pdf.SetFont("Arial", "B", 16)
-	pdf.Cell(0, 10, "Generated Report")
+	pdf.Cell(0, 10, "Generated Client")
 	pdf.Ln(10)
 	pdf.SetFont("Arial", "", 12)
 
-	for _, report := range reports {
-		pdf.Cell(0, 10, fmt.Sprintf("Title: %s", report.Title))
+	for _, client := range clients {
+		pdf.Cell(0, 10, fmt.Sprintf("Name: %s %s", client.ClientShortName,client.ClientLongName))
 		pdf.Ln(8)
-		pdf.MultiCell(0, 10, fmt.Sprintf("Content: %s", report.Content), "TLR", "", false)
+		pdf.MultiCell(0, 10, fmt.Sprintf("ClientEmail: %s", client.ClientEmail), "TLR", "", false)
+		pdf.MultiCell(0, 10, fmt.Sprintf("Salutation: %s", client.Salutation), "TLR", "", false)
+		pdf.MultiCell(0, 10, fmt.Sprintf("Language: %s", client.Language), "TLR", "", false)
+		pdf.MultiCell(0, 10, fmt.Sprintf("Gender: %s", client.Gender), "TLR", "", false)
+		pdf.MultiCell(0, 10, fmt.Sprintf("ClientDob: %s", client.ClientDob), "TLR", "", false)
 		pdf.Ln(8)
 	}
 
@@ -99,12 +115,11 @@ func generatePDFReportHandler(c *gin.Context) {
 }
 
 
-// Function to get all reports
-func getReports(c *gin.Context) ([]Report, error) {
-	var reports []Report
-	result := db.Find(&reports)
+func getClients(c *gin.Context) ([]Client, error) {
+	var client []Client
+	result := db.Find(&client)
 	if result.Error != nil {
 		return nil, result.Error
 	}
-	return reports, nil
+	return client, nil
 }
