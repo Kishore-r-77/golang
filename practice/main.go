d507126d2beadb69a56ee55965f8a01a3770043b
Kishore-r-77 <kishorework77@gmail.com>
2024-01-30 20:49:29 +0530

    practiced switch statement

--- practice/main.go
+++ practice/main.go
@@ -3,7 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var name interface{} = "kishore"
-	name = 24
-	fmt.Println(name)
+	var name string = "Aswathy"
+
+	switch name {
+	case "Kishore":
+		{
+			fmt.Println("Hello Kishore")
+		}
+	case "Aswathy":
+		fmt.Println("Hello Aswathy")
+		fmt.Println("Hello Goddess")
+	default:
+		fmt.Println("Select a Name")
+	}
+
 }
