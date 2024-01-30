6ae6613006b9d2f131ce6a50675846662316816b
Kishore-r-77 <kishorework77@gmail.com>
2024-01-30 20:42:40 +0530

    tested interface{} in golang

--- practice/main.go
+++ practice/main.go
@@ -1,28 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-
-	type fiance struct {
-		FianceName string
-	}
-
-	type Person struct {
-		Name    string
-		Age     int
-		Company string
-		Ash     fiance
-	}
-
-	kishore := Person{}
-	kishore.Name = "Kishore"
-	kishore.Age = 27
-	kishore.Company = "Netflix"
-	kishore.Ash.FianceName = "Aswathy"
-
-	fmt.Println(kishore.Ash.FianceName)
-
+	var name interface{} = "kishore"
+	name = 24
+	fmt.Println(name)
 }
