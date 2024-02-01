4327394fedd0619890177164292d465ef24bcec8
Kishore-r-77 <kishorework77@gmail.com>
2024-02-01 20:49:57 +0530

    user input

--- practice/main.go
+++ practice/main.go
@@ -1,44 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
-
-type Person struct {
-	name string
-	age  int
-}
-
-type Animal struct {
-	name string
-	age  int
-}
-
-type Display interface {
-	makeNoise()
-}
-
-func (person Person) makeNoise() {
-	fmt.Println("Hey Yooooo")
-}
-func (animal Animal) makeNoise() {
-	fmt.Println("Roarrrrr")
-}
-
-func display(d Display) {
-	d.makeNoise()
-}
+import "fmt"
 
 func main() {
 
-	kishore := Person{}
-	kishore.name = "Kishore"
-	kishore.age = 27
+	var name string
 
-	tiger := Animal{}
-	tiger.name = "Tiger"
-	tiger.age = 20
+	fmt.Println("What is you're name")
 
-	display(kishore)
+	fmt.Scan(&name)
+
+	fmt.Printf("Hello %v", name)
 
 }
